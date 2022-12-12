4a8dd58b0c84b82814869b00dd6f3e535d86686d
azamie <ishii.kami2001@gmail.com>
2022-12-12 21:39:21 +0700

    Homepage implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import './App.css'
+import Home from 'pages/Home'
+import {
+  ConnectionProvider,
+  WalletProvider
+} from '@solana/wallet-adapter-react'
+import { PhantomWalletAdapter, Coin98WalletAdapter } from '@solana/wallet-adapter-wallets'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+
+require('@solana/wallet-adapter-react-ui/styles.css')
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <ConnectionProvider endpoint={'https://api.devnet.solana.com'}>
+        <WalletProvider wallets={[new PhantomWalletAdapter(), new Coin98WalletAdapter()]} autoConnect>
+          <WalletModalProvider>
+            <Home />
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App

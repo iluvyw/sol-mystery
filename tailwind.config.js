e8a92f50fac612892c09b02c714837ee58a8a175
azamie <ishii.kami2001@gmail.com>
2022-12-12 20:31:26 +0700

    Add craco config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        outfit: ['"Outfit"']
+      },
+      boxShadow: {
+        button: '2px 2px 4px 2px rgba(0, 0, 0, 0.25)'
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}

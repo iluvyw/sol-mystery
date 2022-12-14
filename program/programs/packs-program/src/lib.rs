eadf24eb67feb5c4562ed38d3bbc136c8edeaa13
Quang Le <quang.chau-nhat.le@aioz.io>
2022-12-14 18:01:42 +0700

    create pack

--- program/programs/packs-program/src/lib.rs
+++ program/programs/packs-program/src/lib.rs
@@ -1,15 +1,42 @@
 use anchor_lang::prelude::*;
+use magic_crypt::{new_magic_crypt, MagicCryptTrait};
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("ASqbFGxqkxMJWcG25pguvvS5p2JAQToR3HLMVV8T3WNL");
 
 #[program]
 pub mod packs_program {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_pack(
+        ctx: Context<CreatePack>,
+        mint0: Pubkey,
+        mint1: Pubkey,
+        mint2: Pubkey,
+    ) -> Result<()> {
+        let pack_pda = &mut ctx.accounts.pack_pda;
+        let secret_key = env!("SECRET_KEY");
+        let magic_crypt = new_magic_crypt!(secret_key, 256);
+
+        pack_pda.mint0 = magic_crypt.encrypt_bytes_to_base64(mint0.as_ref());
+        pack_pda.mint1 = magic_crypt.encrypt_bytes_to_base64(mint1.as_ref());
+        pack_pda.mint2 = magic_crypt.encrypt_bytes_to_base64(mint2.as_ref());
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(mint0: Pubkey, mint1: Pubkey, mint2: Pubkey)]
+pub struct CreatePack<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    #[account(init_if_needed, payer=payer, space=32+32+32+8, seeds=[mint0.as_ref(),mint1.as_ref(),mint2.as_ref()], bump)]
+    pub pack_pda: Account<'info, Pack>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Pack {
+    mint0: [u8; 64],
+    mint1: [u8; 64],
+    mint2: [u8; 64],
+}
--- program/tests/packs-program.ts
+++ program/tests/packs-program.ts
@@ -1,16 +1,48 @@
-import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
-import { PacksProgram } from "../target/types/packs_program";
+import * as anchor from '@project-serum/anchor'
+import { Program } from '@project-serum/anchor'
+import { Keypair } from '@solana/web3.js'
+import { PacksProgram } from '../target/types/packs_program'
 
-describe("packs-program", () => {
+describe('packs-program', () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider)
 
-  const program = anchor.workspace.PacksProgram as Program<PacksProgram>;
+  const program = anchor.workspace.PacksProgram as Program<PacksProgram>
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+  it('Create pack', async () => {
+    // create user
+    const userAccount = Keypair.generate()
+    await provider.connection.requestAirdrop(
+      userAccount.publicKey,
+      1 * anchor.web3.LAMPORTS_PER_SOL
+    )
+    await new Promise((r) => setTimeout(r, 1000))
+    const balance = await provider.connection.getBalance(userAccount.publicKey)
+    console.log({ balance })
+
+    // create mint account
+    const mint0 = Keypair.generate()
+    const mint1 = Keypair.generate()
+    const mint2 = Keypair.generate()
+
+    // create pda
+    const [pda, _] = anchor.web3.PublicKey.findProgramAddressSync(
+      [
+        mint0.publicKey.toBuffer(),
+        mint1.publicKey.toBuffer(),
+        mint2.publicKey.toBuffer()
+      ],
+      program.programId
+    )
+    const tx = await program.methods
+      .createPack(mint0.publicKey, mint1.publicKey, mint2.publicKey)
+      .accounts({
+        payer: userAccount.publicKey,
+        packPda: pda
+      })
+      .signers([userAccount])
+      .rpc()
+    console.log('Your transaction signature', tx)
+  })
+})
